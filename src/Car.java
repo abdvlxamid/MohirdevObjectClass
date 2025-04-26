a1ede373787ed757b576285541c4256acf535a8f
abdvlxamid <emilioiesco@gmail.com>
2025-04-26 18:56:08 +0500

    commited

---
+++ src/Car.java
@@ -0,0 +1,9 @@
+public class Car {
+
+    public void getColor(String color){
+     System.out.println(color);
+    }
+    public void getModel(String model){
+        System.out.println(model);
+    }
+}
---
+++ src/Product.java
@@ -0,0 +1,11 @@
+public class Product {
+    public static void main(String[] args) {
+        Car mersedes = new Car();
+        mersedes.getColor("green");
+        mersedes.getModel("mercedes");
+
+        Car bmw = new Car();
+        bmw.getColor("black");
+        bmw.getModel("M5");
+    }
+}
